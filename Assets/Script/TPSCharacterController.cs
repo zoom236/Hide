61fcce61deaf112a73f77540f6628cce66913b19
DESKTOP-TOV6GCB\HyunYoung <hyenyoung29@gmail.com>
2022-03-03 23:41:03 +0900

    player move

---
+++ Assets/Script/TPSCharacterController.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TPSCharacterController : MonoBehaviour
+{
+    [SerializeField]
+    private Transform characterBody;
+    [SerializeField]
+    private Transform cameraArm;
+
+    Animator animator;
+
+    private int toggleView = 3; // 1=1ÀÎÄª, 3=3ÀÎÄª
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = characterBody.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LookAround();
+        Move();
+    }
+
+    private void Move()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        bool isMove = moveInput.magnitude != 0;
+        animator.SetBool("isMove", isMove);
+        if (isMove)
+        {
+            Vector3 lookForward = new Vector3(cameraArm.forward.x, 0f, cameraArm.forward.z).normalized;
+            Vector3 lookRight = new Vector3(cameraArm.right.x, 0f, cameraArm.right.z).normalized;
+            Vector3 moveDir = lookForward * moveInput.y + lookRight * moveInput.x;
+
+            characterBody.forward = moveDir;
+            //characterBody.forward = lookForward;
+            transform.position += moveDir * Time.deltaTime * 5f;
+        }
+
+        //Debug.DrawRay(cameraArm.position, new Vector3(cameraArm.forward.x, 0f, cameraArm.forward.z).normalized, Color.red);
+    }
+
+    private void LookAround()
+    {
+        Vector2 mouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+        Vector3 camAngle = cameraArm.rotation.eulerAngles;
+        float x = camAngle.x - mouseDelta.y;
+
+        if(x < 180f)
+        {
+            x = Mathf.Clamp(x, -1f, 70f);
+        }
+        else
+        {
+            x = Mathf.Clamp(x, 355f, 361f);
+        }
+
+        cameraArm.rotation = Quaternion.Euler(x, camAngle.y + mouseDelta.x, camAngle.z);
+
+    }
+}
