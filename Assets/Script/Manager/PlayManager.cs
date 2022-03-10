706ddaed925a0e81601f8f1ce0fef9f991a4c5dd
goni0309 <goni0309@gmail.com>
2022-03-11 02:12:55 +0900

    PlayManager 작업환경

---
+++ Assets/Script/Manager/PlayManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayManager : MonoBehaviour
+{
+    public static PlayManager instance { get; private set; }
+    private void Awake() =>  instance = this;
+
+    delegate void GameStart();
+    GameStart GS;
+
+    //delegate int 
+
+
+
+
+    static class YieldCash
+    {
+        public static readonly WaitForEndOfFrame EOF = new WaitForEndOfFrame();
+        public static readonly WaitForFixedUpdate FixdUpdate = new WaitForFixedUpdate();
+        public static readonly WaitForSeconds Seconds001 = new WaitForSeconds(0.1f);
+        public static readonly WaitForSeconds Seconds01 = new WaitForSeconds(1);
+    }
+
+
+    //코루틴
+
+
+    //외부참조
+
+
+}
