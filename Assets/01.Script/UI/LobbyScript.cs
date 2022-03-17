860cf3bbabf41082e83f7724157ea277b3000837
DESKTOP-TOV6GCB\HyunYoung <hyenyoung29@gmail.com>
2022-03-17 11:31:10 +0900

    InvenTest

---
+++ Assets/01.Script/UI/LobbyScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LobbyScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+  
+
+    public void LobbyScriptBtn()
+    {
+        switch (this.gameObject.name)
+        {
+                case "RoomMakeBtn":
+                SceneManager.LoadScene("GameRoom");
+                break;
+        }
+    }
+}
