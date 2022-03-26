11bb22fccfc090add4cef5952270aeb9c96588b1
zoom2 <zoom_2@Naver.com>
2022-03-26 17:27:31 +0900

    플레이어 동기화 수정

--- Assets/Script/Namul/PlayerScript.cs
+++ Assets/Script/Namul/PlayerScript.cs
@@ -7,22 +7,17 @@ using UnityEngine.UI;
 
 public class PlayerScript : MonoBehaviourPunCallbacks, IPunObservable
 {
-    [SerializeField]
-    private Transform characterBody;    // 캐릭터
-    [SerializeField]
-    private Transform cameraArm;        // 카메라 회전
-
-    Animator anim;
+    Animator animator;
+    Camera camera;
     Rigidbody rigid;
 
-    bool jDown;  // 점프
-    bool isJump;
+    public float speed = 5f;
+    public float runSpeed = 8f;
+    public float finalSpeed;
+    public bool toggleCameraRotation;
+    public bool run;
 
-    bool sDown;  // 앉기
-    bool isSit;
-
-    bool tDown;  // 일어나기
-    bool isStand;
+    public float smoothness = 10f;
 
     public Rigidbody RB;
     public Animator AN;
@@ -31,23 +26,11 @@ public class PlayerScript : MonoBehaviourPunCallbacks, IPunObservable
     public Text NickNameText;
     //public Image HealthImage;
 
-    Vector3 curPos;
-
-    //bool swDown;  // 앉은상태에서 걷기
-    //bool isSitWalk;
-
-    //RedBeanSpawn redbeanspawn;      // 던지는 유무확인
-
-    //private int ToggleView = 3; // 1=1인칭, 3=3인칭
-
     void Awake()
     {
-        anim = characterBody.GetComponent<Animator>();        // characterBody.GetComponent
-        rigid = characterBody.GetComponent<Rigidbody>();
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
-
-        //redbeanspawn = GameObject.Find("Spawn").GetComponent<RedBeanSpawn>();
+        animator = this.GetComponent<Animator>();
+        camera = Camera.main;
+        rigid = this.GetComponent<Rigidbody>();
 
         NickNameText.text = PV.IsMine ? PhotonNetwork.NickName : PV.Owner.NickName;
         NickNameText.color = PV.IsMine ? Color.green : Color.red;
@@ -55,151 +38,61 @@ public class PlayerScript : MonoBehaviourPunCallbacks, IPunObservable
 
     void Update()
     {
-        GetInput();     // 키 입력
-        Move();         // 플레이어 이동
-        LookAround();   // 카메라 시선 이동
-        SitMove();      // 앉아서 이동
-        Jump();         // 점프
-        Sit();          // 앉기
-        Stand();        // 일어나기
-        Throw();        // 던지기
-    }
-
-    void GetInput()
-    {
-        jDown = Input.GetButtonDown("Jump");
-        sDown = Input.GetKeyDown(KeyCode.LeftControl);
-
-        if (sDown && isSit)
+        if (Input.GetKey(KeyCode.LeftAlt))
         {
-            tDown = Input.GetKeyDown(KeyCode.LeftControl);
+            toggleCameraRotation = true;        // 둘러보기 활성화
         }
+        else
+        {
+            toggleCameraRotation = false;       // 둘러보기 비활성화
+        }
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            run = true;
+        }
+        else
+        {
+            run = false;
+        }
+        Move();
 
-        if(Input.GetKeyDown(KeyCode.F))
+        if (Input.GetKeyDown(KeyCode.F))
         {
             Smoke();
         }
     }
 
+
+    void LateUpdate()
+    {
+        if (toggleCameraRotation != true)
+        {
+            Vector3 playerRotate = Vector3.Scale(camera.transform.forward, new Vector3(1, 0, 1));
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(playerRotate), Time.deltaTime * smoothness);
+        }
+    }
     void Smoke()
     {
         PhotonNetwork.Instantiate("SmokeGrenade", transform.position, Quaternion.identity);
     }
 
-    private void Move()
+    void Move()
     {
+        finalSpeed = (run) ? runSpeed : speed;
+
         Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         bool isMove = moveInput.magnitude != 0;   // moveInput이 0이면 이동입력이 없는것
-        anim.SetBool("isRun", isMove);
 
         if (isMove)
         {
-            Vector3 lookForward = new Vector3(cameraArm.forward.x, 0f, cameraArm.forward.z).normalized;
-            Vector3 lookRight = new Vector3(cameraArm.right.x, 0f, cameraArm.right.z).normalized;
-            Vector3 moveDir = lookForward * moveInput.y + lookRight * moveInput.x;
+            Vector3 forward = transform.TransformDirection(Vector3.forward);
+            Vector3 right = transform.TransformDirection(Vector3.right);
+            Vector3 moveDirection = forward * moveInput.y + right * moveInput.x;
 
-            //characterBody.forward = moveDir;
-            characterBody.forward = lookForward;
-            transform.position += moveDir * Time.deltaTime * 5f;
+            transform.position += moveDirection * Time.deltaTime * 5f;
 
-            //if (Input.GetKeyDown(KeyCode.LeftAlt))
-            //{
-            //    characterBody.forward = lookForward;
-            //}
-            //else
-            //{
-            //    characterBody.forward = moveDir;
-            //}
-        }
-
-
-        //Debug.DrawRay(cameraArm.position, new Vector3(cameraArm.forward.x, 0f, cameraArm.forward.z).normalized, Color.red);
-    }
-    void Sit()
-    {
-        if (sDown && !isSit)
-        {
-            anim.SetBool("isSit", true);
-            isSit = true;
-        }
-        //isSit = false;
-    }
-
-    void SitMove()
-    {
-        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        bool isMove = moveInput.magnitude != 0;   // moveInput이 0이면 이동입력이 없는것
-        anim.SetBool("isSitWalk", isMove);
-
-        if (isMove)
-        {
-            Vector3 lookForward = new Vector3(cameraArm.forward.x, 0f, cameraArm.forward.z).normalized;
-            Vector3 lookRight = new Vector3(cameraArm.right.x, 0f, cameraArm.right.z).normalized;
-            Vector3 moveDir = lookForward * moveInput.y + lookRight * moveInput.x;
-
-            characterBody.forward = moveDir;
-            //characterBody.forward = lookForward;
-            transform.position += moveDir * Time.deltaTime * 0.5f;
-        }
-    }
-
-    void Stand()
-    {
-        if (tDown && isSit && !isStand)
-        {
-            anim.SetBool("isStand", true);
-            isStand = true;
-        }
-    }
-
-    void Jump()
-    {
-        if (jDown && !isJump)
-        {
-            rigid.AddForce(Vector3.up * 5, ForceMode.Impulse);
-            //anim.SetBool("isJump", true);
-            anim.SetTrigger("doJump");
-            isJump = true;
-        }
-    }
-
-    void Throw()
-    {
-        if (GameObject.Find("Spawn").GetComponent<RedBeanSpawn>().RedBeanUse == true)
-        {
-            Debug.Log("던지기");
-            anim.SetTrigger("doThrow");
-        }
-        
-    }
-
-    private void LookAround()       // 카메라 회전 기능
-    {
-        //Debug.DrawRay(cameraArm.position, cameraArm.forward * 100f, Color.red);
-        Vector2 mouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));       // mouse x = 마우스 좌우 수치, mouse y = 마우스 상하 수치 
-        Vector3 camAngle = cameraArm.rotation.eulerAngles;
-        float x = camAngle.x - mouseDelta.y;
-
-        if(x < 180f)
-        {
-            x = Mathf.Clamp(x, -1f, 70f);
-        }
-        else
-        {
-            x = Mathf.Clamp(x, 335f, 361f);
-        }
-
-        cameraArm.rotation = Quaternion.Euler(x, camAngle.y + mouseDelta.x, camAngle.z);
-    }
-    void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.tag == "Floor")
-        {
-            Debug.Log("착지");
-            //anim.SetBool("isJump", false);
-            isJump = false;
-            isSit = false;
-            isStand = false;
+            float percent = ((run) ? 1 : 0.5f) * moveDirection.magnitude;
+            animator.SetFloat("Blend", percent, 0.1f, Time.deltaTime);
         }
     }
 
